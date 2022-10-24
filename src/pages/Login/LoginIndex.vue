98334e7af490c6aa99eb101eae7a96895b35fbc6
flingyp <1213563369@qq.com>
2022-10-24 11:41:06 +0800

    feat(projects): login静态页面响应式优化

--- src/pages/Login/LoginIndex.vue
+++ src/pages/Login/LoginIndex.vue
@@ -5,19 +5,31 @@
         <SvgIcon name="LoginCover"></SvgIcon>
       </div>
       <div class="form-container-right">
-        <h1>YRoam Admin 后台管理系统</h1>
-        <n-form class="form-container" ref="formRef" :label-width="80" :model="formValue" :rules="rules" :size="size">
-          <n-form-item label="姓名" path="user.name">
-            <n-input v-model:value="formValue.user.name" placeholder="输入姓名" />
+        <h1>
+          <img src="@assets/Img/YRoamLogo.png" alt="YRoam Logo" />
+          <span>YRoam Admin 后台管理系统</span>
+        </h1>
+        <n-form
+          class="form-container"
+          ref="formRef"
+          :label-width="80"
+          :model="LoginFormData"
+          :rules="LoginRules"
+          :size="FormSize"
+        >
+          <n-form-item path="username">
+            <n-input v-model:value="LoginFormData.username" placeholder="请输入个人账号："></n-input>
           </n-form-item>
-          <n-form-item label="年龄" path="user.age">
-            <n-input v-model:value="formValue.user.age" placeholder="输入年龄" />
-          </n-form-item>
-          <n-form-item label="电话号码" path="phone">
-            <n-input v-model:value="formValue.phone" placeholder="电话号码" />
+          <n-form-item path="password">
+            <n-input
+              v-model:value="LoginFormData.password"
+              placeholder="请输入密码："
+              type="password"
+              show-password-on="click"
+            ></n-input>
           </n-form-item>
           <n-form-item>
-            <n-button attr-type="button" @click="handleValidateClick"> 验证 </n-button>
+            <n-button type="primary" @click="handlerUserLogin" style="width: 100%">登录</n-button>
           </n-form-item>
         </n-form>
       </div>
@@ -26,51 +38,44 @@
 </template>
 
 <script setup lang="ts">
-  import { FormInst, NButton, NForm, NFormItem, NInput, useMessage } from 'naive-ui'
-  import { ref } from 'vue'
+  import { FormInst, FormRules, NButton, NForm, NFormItem, NInput } from 'naive-ui'
+  import { reactive, ref } from 'vue'
 
-  const size = ref<'small' | 'medium' | 'large'>('medium')
-  const formRef = ref<FormInst | null>(null)
-  const message = useMessage()
-  const formValue = ref({
-    user: {
-      name: '',
-      age: ''
-    },
-    phone: ''
+  const LoginFormData = reactive({
+    username: 'admin',
+    password: '123456'
   })
-  const rules = {
-    user: {
-      name: {
+
+  const LoginRules: FormRules = {
+    username: [
+      {
         required: true,
-        message: '请输入姓名',
-        trigger: 'blur'
-      },
-      age: {
-        required: true,
-        message: '请输入年龄',
+        message: '请输入用户名',
         trigger: ['input', 'blur']
       }
-    },
-    phone: {
-      required: true,
-      message: '请输入电话号码',
-      trigger: ['input']
-    }
-  }
-  const handleValidateClick = (e: MouseEvent) => {
-    e.preventDefault()
-    formRef.value?.validate(errors => {
-      if (!errors) {
-        message.success('Valid')
-      } else {
-        console.log(errors)
-        message.error('Invalid')
+    ],
+    password: [
+      {
+        required: true,
+        message: '请输入密码',
+        trigger: ['blur']
       }
+    ]
+  }
+  const FormSize = ref<'small' | 'medium' | 'large'>('large')
+  const formRef = ref<FormInst | null>(null)
+
+  const handlerUserLogin = () => {
+    // 验证
+    formRef.value?.validate(async errors => {
+      // if (errors) return
     })
   }
 </script>
 
 <style scoped lang="scss">
+  .n-form-item {
+    display: inline;
+  }
   @import './Login.scss';
 </style>
