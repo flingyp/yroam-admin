65bc08c6226b0bb0d0557032c368576ccf46f6c4
flingyp <1213563369@qq.com>
2022-10-31 18:02:42 +0800

    feat(projects): 引入nprogress进度条

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,10 @@
+import NProgress from 'nprogress'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import GlobalRouterHandleProcess from './GlobalRouterHandleProcess'
 import { CONSTANT_ROUTERS } from './modules/CONSTANT_ROUTERS'
 import { transformSystemRouteToRouteRecordRaw } from './utils'
+
+NProgress.configure({ easing: 'ease', speed: 500, trickle: false })
 
 export const routerInstance = createRouter({
   history: createWebHashHistory(),
@@ -9,5 +12,10 @@ export const routerInstance = createRouter({
 })
 
 routerInstance.beforeEach(async (to, from, next) => {
+  NProgress.start()
   await GlobalRouterHandleProcess(to, from, next, routerInstance)
+})
+
+routerInstance.afterEach(async () => {
+  NProgress.done()
 })
