e919863cb45d90c5b1c766e81d38532be4952f2f
flingyp <1213563369@qq.com>
2022-11-05 11:20:50 +0800

    fix(projects): 顶部菜单混合LogoTitle组件宽度与侧边栏宽度自适应

--- src/layout/DifLayoutMode/TopSiderMode/TopSiderHeader.vue
+++ src/layout/DifLayoutMode/TopSiderMode/TopSiderHeader.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="top-sider-header-container">
     <div class="top-sider-header-left">
-      <GlobalLogoTitle style="margin-right: 70px"></GlobalLogoTitle>
+      <GlobalLogoTitle :style="{ width: SystemConfigStore.SiderWidth + 'px' }">
+      </GlobalLogoTitle>
       <GlobalBreadCrumb></GlobalBreadCrumb>
     </div>
     <div class="top-sider-header-right">
@@ -20,32 +21,36 @@ import GlobalLogoTitle from '@layout/LayoutComponent/GlobalLogoTitle.vue';
 import GlobalSettingIcon from '@layout/LayoutComponent/GlobalSettingIcon.vue';
 import GlobalSiderCollapse from '@layout/LayoutComponent/GlobalSiderCollapse.vue';
 import GlobalUserCenter from '@layout/LayoutComponent/GlobalUserCenter.vue';
+
+import { useSystemConfigStore } from '@store/index';
+
+const SystemConfigStore = useSystemConfigStore();
 </script>
 
 <style scoped lang="scss">
-  .top-sider-header-container {
-    box-sizing: border-box;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0 20px;
-  }
+.top-sider-header-container {
+  box-sizing: border-box;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 
-  .top-sider-header-left {
-    height: 100%;
-    display: flex;
-    align-items: center;
-  }
+.top-sider-header-left {
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
 
-  .top-sider-header-right {
-    height: 100%;
-    display: flex;
-    align-items: center;
+.top-sider-header-right {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  padding-right: 15px;
 
-    & > *:not(:last-child) {
-      cursor: pointer;
-      margin-right: 20px;
-    }
+  &>*:not(:last-child) {
+    cursor: pointer;
+    margin-right: 20px;
   }
+}
 </style>
