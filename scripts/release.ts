01659246ce9b7df0fc83a67890c1aeb088db9689
flingyp <1213563369@qq.com>
2022-11-29 14:41:38 +0800

    build(projects): 添加release脚本命令

---
+++ scripts/release.ts
@@ -0,0 +1,25 @@
+import { execSync } from 'child_process'
+import chalk from 'chalk'
+import { inquireVersion } from './inquirer'
+
+// NPM 发包流程文件
+// 0. 执行 npm run test 确保所有测试用例通过
+// 1. 询问发什么版本的包 major minor patch\
+/**
+ * standard-version --release-as major
+ * standard-version --release-as minor
+ * standard-version --release-as patch
+ */
+// 2. 执行这个 release:changelog:${version}
+// 3. 发包 npm publish
+// 4. 提交到远程仓库中 git push origin main
+async function initRelease() {
+  const version = await inquireVersion()
+  execSync(`standard-version --release-as ${version}`, { stdio: 'inherit' })
+  console.log(chalk.blueBright('版本更新完成!'))
+  execSync('git push origin main', { stdio: 'inherit' })
+  execSync('git push origin --tags', { stdio: 'inherit' })
+  console.log(chalk.blueBright('代码提交到远程仓库中!'))
+}
+
+initRelease()
